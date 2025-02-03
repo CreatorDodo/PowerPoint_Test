0b929905fc1f535cb23d3c5257ed95293487723b
Kim Do Young <difbfl4750@gmail.com>
2025-02-03 20:57:59 +0900

    config: ESLint & Prettier 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-refresh'],
+  rules: {
+    'tailwindcss/no-custom-classname': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
