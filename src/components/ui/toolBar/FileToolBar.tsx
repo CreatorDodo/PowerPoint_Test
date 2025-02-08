29e8110a34ab9f4977627af655ee1d6d5351ef79
Kim Do Young <difbfl4750@gmail.com>
2025-02-08 19:17:43 +0900

    fix: findDOMNode 경고 해결

--- src/components/ui/toolBar/FileToolBar.tsx
+++ src/components/ui/toolBar/FileToolBar.tsx
@@ -1,15 +1,31 @@
+import { saveAsSVG, saveAsPNG } from '@/utils/fileExport';
 import BaseToolBar from './BaseToolBar';
 import ToolbarSection from './ToolbarSection';
 import ToolbarItem from './ToolbarItem';
 import ToolbarDivider from './ToolbarDivider';
 import { SaveSVGIcon, ImageIcon } from '@/components/icons';
+import { useCanvasStore } from '@/stores/useCanvasStore';
 
 const FileToolBar = () => {
+  const canvasRef = useCanvasStore((state) => state.canvasRef);
+
+  const handleSaveSVG = () => {
+    if (canvasRef?.current) {
+      saveAsSVG(canvasRef.current);
+    }
+  };
+
+  const handleSavePNG = () => {
+    if (canvasRef?.current) {
+      saveAsPNG(canvasRef.current);
+    }
+  };
+
   return (
     <BaseToolBar>
       <ToolbarSection label="저장">
-        <ToolbarItem icon={<SaveSVGIcon />} label="SVG로 저장" />
-        <ToolbarItem icon={<ImageIcon className="stroke-neutral-600" />} label="PNG로 저장" />
+        <ToolbarItem icon={<SaveSVGIcon />} label="SVG로 저장" onClick={handleSaveSVG} />
+        <ToolbarItem icon={<ImageIcon className="stroke-neutral-600" />} label="PNG로 저장" onClick={handleSavePNG} />
       </ToolbarSection>
       <ToolbarDivider />
     </BaseToolBar>
